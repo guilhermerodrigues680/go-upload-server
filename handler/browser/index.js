3d78230c8f9040f1e1757c8056bf191a8d9cd802
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-10-23 19:35:18 -0300

    Listando arquivos. Inicio da implementação para usar Alpine.js.

--- handler/browser/index.js
+++ handler/browser/index.js
@@ -1,5 +1,3 @@
-console.debug("ok");
-
 const fileForm = document.querySelector("#file-form");
 fileForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
@@ -55,4 +53,84 @@ function formatBytes(bytes, decimals = 2) {
   const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
+
+/**
+ * @param {number} mode
+ * @returns string chmod em octal
+ */
+function formatChmod(mode) {
+  // https://ss64.com/bash/chmod.html
+  // chmod calculator
+  return mode.toString(8);
+}
+
+function formatDate(date) {
+  try {
+    if (date) {
+      return new Date(date).toLocaleString();
+    }
+  } catch (error) {
+    console.error("date parse error:", error);
+  }
+  return "?";
+}
+
+function fileListDataFunc() {
+  return {
+    fileList: null,
+    loading: false,
+    curDir: null,
+
+    init() {
+        this.curDir = this.getCurDir();
+      this.loadDir(this.curDir);
+    },
+
+    getCurDir() {
+      const curUrl = new URL(window.location.toString());
+      const dir = curUrl.searchParams.get("dir");
+      return dir || "/";
+    },
+
+    async handleClickBack() {
+        console.debug("click back")
+        const paths = this.curDir.slice(0, this.curDir.length-1).split("/");
+        paths.pop();
+        newUrl = `${paths.join("/")}/`;
+        await this.loadDir(newUrl);
+        // TODO: usar um watch para curDir? ou centralizar mudanças?
+        this.curDir = newUrl;
+    },
+
+    async handleClickFile(file) {
+        const { name, isDir } = file;
+        console.debug("file", file,name, isDir);
+        // TODO: usar pushState?
+        // https://stackoverflow.com/questions/824349/how-do-i-modify-the-url-without-reloading-the-page
+      if (isDir) {
+        const newUrl = `${this.curDir}${name}/`;
+        await this.loadDir(newUrl);
+        this.curDir = newUrl;
+      } else {
+        const destUrl = new URL(`${this.curDir}${name}`, window.location.toString());
+        window.location.href = destUrl.href;
+      }
+    },
+
+    async loadDir(dir) {
+      const headers = { Accept: "application/json" };
+      try {
+        this.loading = true;
+        const apiRes = await axios.get(dir, { headers });
+        console.debug("apires", apiRes.data);
+        this.fileList = apiRes.data;
+      } catch (error) {
+        console.error("error", error);
+        throw error;
+      } finally {
+        this.loading = false;
+      }
+    },
+  };
 }
